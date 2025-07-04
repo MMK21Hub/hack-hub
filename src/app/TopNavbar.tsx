ba96ced082bc7d9abffc19ae31b0cc02e246594d
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-07-04 13:11:11 +0100

    Add dark theme, customise the navbar

--- src/app/TopNavbar.tsx
+++ src/app/TopNavbar.tsx
@@ -1,3 +1,4 @@
+import { ThemeToggle } from "@/components/theme-toggle"
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -8,18 +9,37 @@ import {
   NavigationMenuTrigger,
   NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
+import Image from "next/image"
 
 export default function TopNavbar() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <header className="flex justify-between mx-2 my-2">
+      <div className="flex items-center">
+        {/* <Image
+          src="/logo.png"
+          alt="Logo"
+          width={40}
+          height={40}
+          className="rounded-full"
+        /> */}
+        <span className="ml-2 text-lg font-bold">Hack Hub</span>
+      </div>
+      <NavigationMenu className="">
+        <NavigationMenuList>
+          {/* <NavigationMenuItem>
+            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <NavigationMenuLink>Link</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem> */}
+          <NavigationMenuItem>
+            <NavigationMenuLink href="/events">Events</NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <div className="justify-self-end">
+        <ThemeToggle />
+      </div>
+    </header>
   )
 }
