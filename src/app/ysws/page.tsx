f30dae830ded8b168c5658aeec14333f128a788d
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-07-04 15:23:49 +0100

    Fetch the real YSWS items

--- src/app/ysws/page.tsx
+++ src/app/ysws/page.tsx
@@ -1,5 +1,7 @@
 import { Card } from "@/components/ui/card"
 import YSWSEventCard from "@/components/ysws-card"
+import { fetchYSWSCatalog } from "../dataGrabber"
+import { use } from "react"
 
 export const metadata = {
   title: "YSWS - Hack Hub",
@@ -24,13 +26,19 @@ const yswsItemsDummy: YSWSEvent[] = [
 ]
 
 export default function YSWS() {
+  const yswsItems = fetchYSWSCatalog().then((data) =>
+    data.limitedTime
+      .concat(data.indefinite)
+      .filter((ysws) => ysws.status !== "ended")
+  )
+
   return (
     <div className="mx-4 mt-4">
       <p className="mb-4">
         View <strong>You Ship We Ship</strong> events here.
       </p>
-      <div className="">
-        {yswsItemsDummy.map((ysws, index) => (
+      <div className="flex flex-col gap-4">
+        {use(yswsItems).map((ysws, index) => (
           <YSWSEventCard key={index} ysws={ysws} />
         ))}
       </div>
